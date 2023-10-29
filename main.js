784b16dd6206b318f8023c2546cd7834de4d224d
ghadah2019 <ghada__o@hotmail.com>
2023-10-29 15:28:13 +0300

    qq

---
+++ main.js
@@ -0,0 +1,65 @@
+var recheight = 4
+var recwidth = 8
+areaOfrectangle = recheight * recwidth 
+console.log("the are of rectangle is:"  + areaOfrectangle);
+
+// ___________________________
+
+// Q2
+
+let Celsius = 33 
+let Fahrenheit = (Celsius * 1.8) + 32 
+console.log("the f = " + Fahrenheit);
+Celsius = (Fahrenheit - 32) * .5556
+console.log("the C = " + Celsius);
+
+
+// q3 --------------------
+
+let hours = 5
+sec = hours * 3600
+console.log("the number of seconds is:" +  sec);
+
+// q4----------------------------
+
+let score = 94
+if (score == 100 || score >= 95)
+{
+console.log("A+");
+} else if (score < 95 && score >= 90)
+{
+    console.log("A");
+}
+else if (score < 90 && score >= 85)
+{
+    console.log("B+");
+}
+
+else if (score < 84 && score >= 80)
+{
+    console.log("B");
+}
+else if (score < 79 && score >= 75)
+{
+    console.log("C+");
+}else if (score < 74 && score >= 60)
+{
+    console.log("C");
+}else if (score < 59 && score >= 55)
+{
+    console.log("D+");
+}
+else if (score < 54 && score >= 50)
+{
+    console.log("D");
+}
+else if (score < 49 && score >= 0)
+{
+    console.log("F");
+}
+
+
+
+
+
+
